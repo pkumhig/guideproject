50dd1638c6afdcd74b9af60c8166117bd4616283
pkumhig <wangpan20020826@126.com>
2021-05-24 10:09:18 +0800

    对页面重新进行了调整,加入了向导图片

---
+++ js/4page.js
@@ -0,0 +1,28 @@
+/**
+ * Created by navy on 2017/6/28.
+ */
+'use strict';
+//点击close按钮,关闭整个页面
+$('.close_img').click(function () {
+    // window.dobot.close();
+});
+$('.dot_fo').addClass('cil_cor');
+//点击左右箭头,实现页面之间的调整
+$('.bgc_blkl').click(function () {
+    window.location.href = "http://localhost:63342/guideproject/index/3page.html";
+});
+$('.bgc_blkr').click(function () {
+    window.location.href = "http://localhost:63342/guideproject/index/5page.html";
+});
+$('.dot_f').click(function () {
+    window.location.href = "../index.html";
+});
+$('.dot_s').click(function () {
+    window.location.href = "http://localhost:63342/guideproject/index/2page.html";
+});
+$('.dot_t').click(function () {
+    window.location.href = "http://localhost:63342/guideproject/index/3page.html";
+});
+$('.dot_fi').click(function () {
+    window.location.href = "http://localhost:63342/guideproject/index/5page.html";
+});
